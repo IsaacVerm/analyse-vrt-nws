80b9a41b66c0f1cc0e6b6a4dbafcb47393f31306
IsaacVerm <isaacverm@gmail.com>
2025-04-09 14:41:42 +0200

    refine prompt based on comparison extracted results with golden standard

--- extract-places.sh
+++ extract-places.sh
@@ -6,6 +6,7 @@ other places (e.g. Pannenhuis which refers to a specific place) shouldn't be ext
 - always use the full name of the place: e.g. say Nederland instead of NL
 - always give the name of the place in Dutch, even if the place name is in a different language in the title
 - if the name of the place is given in adjective form (e.g. Nederlands instead of Nederland), use the noun form (e.g. Nederland instead of Nederlands)
-- name place in organisation (e.g. Gent in UGent) should not be extracted as a place.
+- name place in organisation (e.g. Gent in UGent) should not be extracted as a place. For example Europese Commissie and EU are organisations as well, 
+so in these cases you shouldn't extract Europa as a place.
 - if someone is mentioned by nationality (e.g. Belg meaning someone from Belgium), do extract the place as a country
 "
